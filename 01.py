f544db1ec6aa227099f935958bab115817745123
Sungwoo <swcho@humaxdigital.com>
2015-04-29 17:27:25 +0900

    Initial commit

---
+++ 01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = 'sungwoo'
+
+tuple = (1, 2)
+list = [1, 2]
+dict = {
+    'name': 'sungwoo',
+    'age': '20'
+}
+
+x, y = tuple
+
+print "Start out"
+print tuple
+print x
+print y
+print dict
