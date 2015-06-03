e40218a6426da655ddf1e84273ab1b5e5700921f
swcho <sungwoo.cho.dev@gmail.com>
2015-06-03 18:06:57 +0900

    function

--- 03_function.py
+++ 03_function.py
@@ -1,6 +1,24 @@
-__author__ = 'swcho'
 
-def print_me( s ):
+print __file__
+
+global_variable = 1
+
+def print_me( arg1, arg2 ):
     "This prints a passed string into this function"
-    print s
-    return
+    print "Print arg1: " +  arg1
+    print "Print arg2: " +  arg2
+    concat = arg1 + '+' + arg2
+    return concat
+
+def func1():
+    print 'nothing'
+    func2()
+
+def func2():
+    print 'nothing 2'
+
+print func1
+func1()
+
+ret_value = print_me("this is argument", "this is second argument")
+print "return value: " + ret_value
